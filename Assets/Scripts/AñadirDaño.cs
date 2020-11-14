bbaf3fd54c167ae82e3e908d8fa1f653c7f1c142
dan13lJD <diazdaniel422@gmail.com>
2020-11-13 18:34:14 -0600

    nuevos cambios
    hola, estos son algunos de los cambios que he hecho en los ultimosd dias

---
+++ Assets/Scripts/AñadirDaño.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AñadirDaño : MonoBehaviour
+{
+    public int damage = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //comprobamos si el jugador se ha entrado en la zona de colision del objeto
+        if (collision.CompareTag("Player"))
+        {
+            collision.SendMessageUpwards("AddDamage", damage);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
